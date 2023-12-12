37c5542dc345b0601d8eda704d96bde1d6001e9b
Ilya Ovsenev <ovsenev.ilya@yandex.ru>
2023-12-12 14:24:59 +0300

    task 2

--- FirstTask/CleanerFolder.cs
+++ FirstTask/CleanerFolder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FirstTask
+﻿namespace FirstTask
 {
     public static class CleanerFolder
     {
@@ -93,7 +87,5 @@ namespace FirstTask
             }
 
         }
-
-
     }
 }
